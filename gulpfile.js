3bbe03e03d62f949d8ed74e15c88688621c25957
ZooLee <zzllee92@gmail.com>
2017-02-27 10:34:05 +0100

    made it more awesome

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require("gulp");
-var ts = require("gulp-typescript");
 var webpack = require('webpack-stream');
-var tsProject = ts.createProject("tsconfig.json");
+var run = require('gulp-run');
 
-gulp.task("default", function() {
+gulp.task("build", function() {
     return gulp.src('./src/main.ts')
         .pipe(webpack(require('./webpack.config.js')))
         .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('clean', function() {
+    return gulp.src('dist', { read: false })
+        .pipe(clean());
+});
+
+gulp.task('run', function() {
+    return run('npm start').exec();
 });
